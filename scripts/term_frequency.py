2af68bf26dfa272a64d3c5457a89f37665a9564a
smortime <smortime@asu.edu>
2016-03-19 13:53:02 -0700

    Updated term_freq script

--- scripts/term_frequency.py
+++ scripts/term_frequency.py
@@ -1,7 +1,7 @@
 #Schuyler Mortimer Honors Thesis
 import csv
 from string import punctuation
-from nltk.corpus import stopwords 
+from nltk.corpus import stopwords
 
 def import_words():
     with open('/home/schuyler/Desktop/Honors_Thesis/data_sets/master.csv', 'rb') as data:
@@ -9,7 +9,8 @@ def import_words():
         return_list = []
 
         for row in reader:
-            temp = preprocess(row[1]).split()
+            temp = preprocess(row[1])
+
             for word in temp:
                 return_list.append(word)
 
@@ -17,17 +18,15 @@ def import_words():
 
 def preprocess(tweet):
     stop = stopwords.words('english')
-    processed_tweet = tweet.decode('utf-8').lower()
 
     for p in list(punctuation):
-        processed_tweet = processed_tweet.replace(p,'')
+        tweet = tweet.replace(p, '')
 
-        return processed_tweet
+    processed_tweet = tweet.decode('utf-8').lower().split()
 
-
-    for w in stop:
-        processed_tweet = processed_tweet.replace(" " + w + " ", '')
-
+    processed_tweet = [i for i in processed_tweet if i not in stop]
+    processed_tweet = [i for i in processed_tweet if i not in punctuation]
+    return processed_tweet
 
 def frequency_count(text):
     term_frequency = {}
